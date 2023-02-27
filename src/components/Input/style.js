01c46c9dccea762fa8587fc943176022b64f1b17
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-27 16:19:20 -0300

    Fixed margin on Input component

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -8,13 +8,15 @@ export const Container = styled.div`
   background-color: ${( { theme } ) => theme.COLORS.DARK_900 };
   color: ${( { theme } ) => theme.COLORS.LIGHT_500 };
 
-  margin: .8rem 0 3.2rem 0;
   border-radius: 8px;
 
   > input {
     width: 100%;
     height: 4.8rem;
     padding: 1.2rem 1.4rem;
+
+    display: flex;
+    align-items: center;
 
     background: transparent;
     color: ${( { theme } ) => theme.COLORS.WHITE};
