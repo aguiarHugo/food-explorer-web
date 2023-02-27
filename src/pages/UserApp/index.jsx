115609faabb026b6336c979000f10b3756e36f51
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-27 16:18:36 -0300

    Created page UserApp

---
+++ src/pages/UserApp/index.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+import { Container } from "./style";
+
+export function UserApp(){
+  return(
+    <Container>
+      <Header />
+    </Container>
+  )
+}
