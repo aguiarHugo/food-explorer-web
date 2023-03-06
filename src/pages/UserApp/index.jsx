d276df3acb0509afbd0129267ea7541a0cb1fdde
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-06 14:56:54 -0300

    Added Title component

--- src/pages/UserApp/index.jsx
+++ src/pages/UserApp/index.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header";
+import { Title } from "../../components/Title";
 import { Container } from "./style";
 
 export function UserApp(){
   return(
     <Container>
       <Header />
+      <Title />
       <div className="app__user-content">
         
       </div>
